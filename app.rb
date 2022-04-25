7e6da63f65fa08324f0b923af66351b8cb74e74b
markcodexq <markcodexq@gmail.com>
2022-04-25 14:17:21 +0500

    Add new views

--- app.rb
+++ app.rb
@@ -2,7 +2,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/posts' do
+	erb :posts
+end
+
+get '/new_posts' do
+	erb :newposts
 end
